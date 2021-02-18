dfa1ba553d9e3d3b77479cfd9e27c6715df0312c
Marcin Niestroj <mniestroj0@gmail.com>
2021-02-18 12:07:42 +0100

    wip: samples: hello: working blockwise transfer

--- samples/hello/src/main.c
+++ samples/hello/src/main.c
@@ -19,12 +19,17 @@ LOG_MODULE_REGISTER(golioth_hello, LOG_LEVEL_DBG);
 #define PEER_PORT		5683
 #endif
 #define MAX_COAP_MSG_LEN	256
+#define MAX_COAP_RECV_MSG_LEN	2048
 
 #define TLS_PSK_ID		"qemu-id"
 #define TLS_PSK			"qemu-psk"
 #define NOTLS_ID		"mark-one"
 
 #define PSK_TAG			1
+
+static bool observe_acked;
+static struct coap_packet observe_request;
+static uint8_t observe_data[MAX_COAP_MSG_LEN];
 
 /* CoAP socket fd */
 static int sock;
@@ -72,11 +77,75 @@ free_data:
 	return err;
 }
 
+static int update_ack(struct coap_packet *request)
+{
+	struct coap_block_context block;
+	struct coap_packet reply;
+	unsigned int block2;
+	uint8_t *data;
+	int err;
+
+	data = (uint8_t *)k_malloc(MAX_COAP_MSG_LEN);
+	if (!data) {
+		LOG_ERR("No MEM!");
+		return -ENOMEM;
+	}
+
+	err = coap_ack_init(&reply, request, data, MAX_COAP_MSG_LEN,
+			    COAP_METHOD_GET);
+	if (err) {
+		LOG_ERR("ACK init fail: %d", err);
+		goto free_data;
+	}
+
+	err = coap_packet_append_option(&reply, COAP_OPTION_URI_PATH,
+					"update", strlen("update"));
+	if (err) {
+		LOG_ERR("append uri: %d", err);
+		goto free_data;
+	}
+
+	block2 = coap_get_option_int(request, COAP_OPTION_BLOCK2);
+
+	err = coap_append_option_int(&reply, COAP_OPTION_BLOCK2, block2 + 0x10);
+	if (err) {
+		LOG_ERR("append block2: %d", err);
+		goto free_data;
+	}
+
+	/* err = coap_update_from_block(request, &block); */
+	/* if (err) { */
+	/* 	LOG_ERR("Update from block fail: %d", err); */
+	/* 	goto free_data; */
+	/* } */
+
+	/* coap_next_block(&reply, &block); */
+
+	err = coap_send(&reply);
+	if (err) {
+		LOG_ERR("Send: %d", err);
+		goto free_data;
+	}
+
+free_data:
+	k_free(data);
+
+	if (err) {
+		LOG_ERR("update err: %d", err);
+	}
+
+	return err;
+}
+
 static int wait_for_rx(void)
 {
 	struct coap_packet rx;
 	uint16_t payload_len;
 	const uint8_t *payload;
+	uint8_t token[COAP_TOKEN_MAX_LEN];
+	size_t token_len;
+	uint8_t observe_token[COAP_TOKEN_MAX_LEN];
+	size_t observe_token_len;
 	uint8_t *data;
 	int rcvd;
 	uint8_t type;
@@ -86,12 +155,12 @@ static int wait_for_rx(void)
 		LOG_ERR("Error in poll:%d", errno);
 	}
 
-	data = (uint8_t *)k_malloc(MAX_COAP_MSG_LEN);
+	data = (uint8_t *)k_malloc(MAX_COAP_RECV_MSG_LEN);
 	if (!data) {
 		return -ENOMEM;
 	}
 
-	rcvd = recv(sock, data, MAX_COAP_MSG_LEN, MSG_DONTWAIT);
+	rcvd = recv(sock, data, MAX_COAP_RECV_MSG_LEN, MSG_DONTWAIT);
 	if (rcvd == 0) {
 		ret = -EIO;
 		goto end;
@@ -117,12 +186,33 @@ static int wait_for_rx(void)
 	type = coap_header_get_type(&rx);
 	payload = coap_packet_get_payload(&rx, &payload_len);
 
-	if (type == COAP_TYPE_CON) {
+	token_len = coap_header_get_token(&rx, token);
+	observe_token_len = coap_header_get_token(&observe_request,
+						  observe_token);
+
+	switch (type) {
+	case COAP_TYPE_CON:
 		if (payload_len == 0) {
 			LOG_INF("PING received");
 		}
 
-		send_ack(&rx);
+		if (token_len == observe_token_len &&
+		    memcmp(token, observe_token, token_len) == 0) {
+			LOG_INF("Update");
+			update_ack(&rx);
+		} else {
+			send_ack(&rx);
+		}
+		break;
+	case COAP_TYPE_ACK:
+		break;
+	case COAP_TYPE_NON_CON:
+		if (token_len == observe_token_len &&
+		    memcmp(token, observe_token, token_len) == 0) {
+			LOG_INF("ACK observe");
+			observe_acked = true;
+		}
+		break;
 	}
 
 	if (payload) {
@@ -155,6 +245,40 @@ static void prepare_fds(void)
 	fds[nfds].fd = sock;
 	fds[nfds].events = POLLIN;
 	nfds++;
+}
+
+static int send_observe_request(void)
+{
+	struct coap_packet *request = &observe_request;
+	uint8_t *data = observe_data;
+	int r;
+
+	/* REVIEW: why is that NON_CON? */
+	r = coap_packet_init(request, data, MAX_COAP_MSG_LEN,
+			     COAP_VERSION_1, COAP_TYPE_NON_CON,
+			     COAP_TOKEN_MAX_LEN, coap_next_token(),
+			     COAP_METHOD_GET, coap_next_id());
+	if (r < 0) {
+		LOG_ERR("Failed to init CoAP message");
+		return r;
+	}
+
+	r = coap_append_option_int(request, COAP_OPTION_OBSERVE, 0);
+	if (r < 0) {
+		LOG_ERR("Failed to append Observe option");
+		return r;
+	}
+
+	r = coap_packet_append_option(request, COAP_OPTION_URI_PATH,
+				      "update", strlen("update"));
+	if (r < 0) {
+		LOG_ERR("Unable add option to request");
+		return r;
+	}
+
+	coap_send(request);
+
+	return r;
 }
 
 static int start_coap_client(void)
@@ -288,9 +412,18 @@ void main(void)
 	k_thread_start(coap_rx);
 
 	while (true) {
-		r = send_hello();
-		if (r < 0) {
-			goto quit;
+		if (0) {
+			r = send_hello();
+			if (r < 0) {
+				goto quit;
+			}
+		}
+
+		if (!observe_acked) {
+			r = send_observe_request();
+			if (r < 0) {
+				goto quit;
+			}
 		}
 
 		k_sleep(K_SECONDS(5));
