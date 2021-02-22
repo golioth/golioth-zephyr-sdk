7aaff93809fc7ba9d20032cbd3419fb83ecde3b6
Marcin Niestroj <mniestroj0@gmail.com>
2021-02-22 15:22:21 +0100

    samples: hello: rework sample to use golioth library
    Golioth library is supposed to take care of most low level communication
    details, such as ACKing pings and calling user registered callbacks when
    some relevant data is received.

--- samples/hello/src/main.c
+++ samples/hello/src/main.c
@@ -11,6 +11,7 @@ LOG_MODULE_REGISTER(golioth_hello, LOG_LEVEL_DBG);
 #include <errno.h>
 #include <net/socket.h>
 #include <net/coap.h>
+#include <net/golioth.h>
 #include <net/tls_credentials.h>
 
 #if defined(CONFIG_NET_SOCKETS_SOCKOPT_TLS)
@@ -26,113 +27,22 @@ LOG_MODULE_REGISTER(golioth_hello, LOG_LEVEL_DBG);
 
 #define PSK_TAG			1
 
-/* CoAP socket fd */
-static int sock;
+/* Golioth instance */
+static struct golioth_client g_client;
+static struct golioth_client *client = &g_client;
+
+static uint8_t rx_buffer[MAX_COAP_MSG_LEN];
 
 struct pollfd fds[1];
 static int nfds;
 
-static int coap_send(struct coap_packet *p)
-{
-	int ret;
-
-	LOG_HEXDUMP_DBG(p->data, p->offset, "TX");
-
-	ret = send(sock, p->data, p->offset, 0);
-	if (ret < 0) {
-		LOG_ERR("Failed to send: %d", -errno);
-		return -errno;
-	}
-
-	return 0;
-}
-
-static int send_ack(struct coap_packet *request)
-{
-	struct coap_packet reply;
-	uint8_t *data;
-	int err;
-
-	data = (uint8_t *)k_malloc(MAX_COAP_MSG_LEN);
-	if (!data) {
-		return -ENOMEM;
-	}
-
-	err = coap_ack_init(&reply, request, data, MAX_COAP_MSG_LEN,
-			    COAP_CODE_EMPTY);
-	if (err) {
-		goto free_data;
-	}
-
-	err = coap_send(&reply);
-
-free_data:
-	k_free(data);
-
-	return err;
-}
-
 static int wait_for_rx(void)
 {
-	struct coap_packet rx;
-	uint16_t payload_len;
-	const uint8_t *payload;
-	uint8_t *data;
-	int rcvd;
-	uint8_t type;
-	int ret = 0;
-
 	if (poll(fds, nfds, 5000) < 0) {
 		LOG_ERR("Error in poll:%d", errno);
 	}
 
-	data = (uint8_t *)k_malloc(MAX_COAP_MSG_LEN);
-	if (!data) {
-		return -ENOMEM;
-	}
-
-	rcvd = recv(sock, data, MAX_COAP_MSG_LEN, MSG_DONTWAIT);
-	if (rcvd == 0) {
-		ret = -EIO;
-		goto end;
-	}
-
-	if (rcvd < 0) {
-		if (errno == EAGAIN || errno == EWOULDBLOCK) {
-			ret = 0;
-		} else {
-			ret = -errno;
-		}
-
-		goto end;
-	}
-
-	LOG_HEXDUMP_DBG(data, rcvd, "RX");
-
-	ret = coap_packet_parse(&rx, data, rcvd, NULL, 0);
-	if (ret < 0) {
-		LOG_ERR("Invalid data received");
-	}
-
-	type = coap_header_get_type(&rx);
-	payload = coap_packet_get_payload(&rx, &payload_len);
-
-	if (type == COAP_TYPE_CON) {
-		if (payload_len == 0) {
-			LOG_INF("PING received");
-		}
-
-		send_ack(&rx);
-	}
-
-	if (payload) {
-		LOG_HEXDUMP_DBG(payload, payload_len, "Payload");
-	}
-
-end:
-	k_free(data);
-
-	return ret;
+	return golioth_process_rx(client);
 }
 
 static void coap_receive(void *arg1, void *arg2, void *arg3)
@@ -152,17 +62,54 @@ K_THREAD_DEFINE(coap_rx, 1024, coap_receive, NULL, NULL, NULL,
 
 static void prepare_fds(void)
 {
-	fds[nfds].fd = sock;
+	fds[nfds].fd = client->sock;
 	fds[nfds].events = POLLIN;
 	nfds++;
+}
+
+static void golioth_on_message(struct golioth_client *client,
+			       struct coap_packet *rx)
+{
+	uint16_t payload_len;
+	const uint8_t *payload;
+	uint8_t type;
+
+	type = coap_header_get_type(rx);
+	payload = coap_packet_get_payload(rx, &payload_len);
+
+	if (payload) {
+		LOG_HEXDUMP_DBG(payload, payload_len, "Payload");
+	}
 }
 
 static int start_coap_client(void)
 {
 	struct sockaddr_in addr4;
-	int proto = IS_ENABLED(CONFIG_NET_SOCKETS_SOCKOPT_TLS) ?
-		IPPROTO_DTLS_1_2 : IPPROTO_UDP;
-	int ret;
+	sec_tag_t sec_tag_list[] = {
+#if defined(CONFIG_NET_SOCKETS_SOCKOPT_TLS)
+		PSK_TAG,
+#endif /* defined(CONFIG_NET_SOCKETS_SOCKOPT_TLS) */
+	};
+	int err;
+
+	golioth_init(client);
+
+	client->rx_buffer = rx_buffer;
+	client->rx_buffer_len = sizeof(rx_buffer);
+
+	client->on_message = golioth_on_message;
+
+	if (IS_ENABLED(CONFIG_NET_SOCKETS_SOCKOPT_TLS)) {
+		err = golioth_set_proto_coap_dtls(client, sec_tag_list,
+						  ARRAY_SIZE(sec_tag_list));
+	} else {
+		err = golioth_set_proto_coap_udp(client, NOTLS_ID,
+						 sizeof(NOTLS_ID) - 1);
+	}
+	if (err) {
+		LOG_ERR("Failed to set protocol: %d", err);
+		return err;
+	}
 
 	addr4.sin_family = AF_INET;
 	addr4.sin_port = htons(PEER_PORT);
@@ -170,77 +117,15 @@ static int start_coap_client(void)
 	inet_pton(addr4.sin_family, CONFIG_NET_CONFIG_PEER_IPV4_ADDR,
 		  &addr4.sin_addr);
 
-	sock = socket(addr4.sin_family, SOCK_DGRAM, proto);
-	if (sock < 0) {
-		LOG_ERR("Failed to create UDP socket %d", errno);
-		return -errno;
-	}
+	client->server = (struct sockaddr *)&addr4;
 
-#if defined(CONFIG_NET_SOCKETS_SOCKOPT_TLS)
-	sec_tag_t sec_tag_list[] = {
-		PSK_TAG,
-	};
-
-	ret = setsockopt(sock, SOL_TLS, TLS_SEC_TAG_LIST,
-			 sec_tag_list, sizeof(sec_tag_list));
-	if (ret < 0) {
-		LOG_ERR("Failed to set TLS_SEC_TAG_LIST option: %d", errno);
-		return -errno;
-	}
-#endif /* defined(CONFIG_NET_SOCKETS_SOCKOPT_TLS) */
-
-	ret = connect(sock, (struct sockaddr *)&addr4, sizeof(addr4));
-	if (ret < 0) {
-		LOG_ERR("Cannot connect to UDP remote : %d", errno);
-		return -errno;
+	err = golioth_connect(client);
+	if (err) {
+		LOG_ERR("Failed to connect: %d", err);
+		return err;
 	}
 
 	prepare_fds();
-
-	return 0;
-}
-
-static int send_hello(void)
-{
-	struct coap_packet request;
-	uint8_t *data;
-	int r;
-
-	data = (uint8_t *)k_malloc(MAX_COAP_MSG_LEN);
-	if (!data) {
-		return -ENOMEM;
-	}
-
-	r = coap_packet_init(&request, data, MAX_COAP_MSG_LEN,
-			     COAP_VERSION_1, COAP_TYPE_CON, COAP_TOKEN_MAX_LEN,
-			     coap_next_token(), COAP_METHOD_GET,
-			     coap_next_id());
-	if (r < 0) {
-		LOG_ERR("Failed to init CoAP message");
-		goto end;
-	}
-
-	r = coap_packet_append_option(&request, COAP_OPTION_URI_PATH,
-				      "hello", strlen("hello"));
-	if (r < 0) {
-		LOG_ERR("Unable add option to request");
-		goto end;
-	}
-
-	if (!IS_ENABLED(CONFIG_NET_SOCKETS_SOCKOPT_TLS)) {
-		r = coap_packet_append_option(&request, COAP_OPTION_URI_QUERY,
-					      "id="NOTLS_ID,
-					      sizeof("id="NOTLS_ID) - 1);
-		if (r < 0) {
-			LOG_ERR("Unable add option to request");
-			goto end;
-		}
-	}
-
-	coap_send(&request);
-
-end:
-	k_free(data);
 
 	return 0;
 }
@@ -288,9 +173,9 @@ void main(void)
 	k_thread_start(coap_rx);
 
 	while (true) {
-		r = send_hello();
+		r = golioth_send_hello(client);
 		if (r < 0) {
-			goto quit;
+			goto disconnect;
 		}
 
 		k_sleep(K_SECONDS(5));
@@ -298,8 +183,9 @@ void main(void)
 
 	LOG_DBG("Done");
 
-quit:
-	(void)close(sock);
+disconnect:
+	golioth_disconnect(client);
 
+quit:
 	LOG_DBG("Quit");
 }
