0dbad8f44eef5fbd68ad06586645db7ca6904621
Marcin Niestroj <m.niestroj@emb.dev>
2021-03-15 22:00:54 +0100

    net: golioth: drop dependency on posix socket API
    Use ZSOCK_MSG_DONTWAIT instead of MSG_DONTWAIT, so that
    CONFIG_NET_SOCKETS_POSIX_NAMES doesn't have to be selected.

--- net/golioth/golioth.c
+++ net/golioth/golioth.c
@@ -403,7 +403,7 @@ int golioth_process_rx(struct golioth_client *client)
 	int err;
 
 	ret = golioth_recv(client, client->rx_buffer, client->rx_buffer_len,
-			   MSG_DONTWAIT);
+			   ZSOCK_MSG_DONTWAIT);
 	if (ret == -EAGAIN || ret == -EWOULDBLOCK) {
 		/* no pending data */
 		return 0;
