c6aa99fd110eed6fcaa892aeb044ef08120c1bad
Marcin Niestroj <m.niestroj@emb.dev>
2021-04-27 14:38:48 +0200

    net: golioth: change Kconfig option NET_GOLIOTH to GOLIOTH
    Remove NET_ prefix, so that all dependent options will have shorter
    names.

--- net/golioth/golioth.c
+++ net/golioth/golioth.c
@@ -10,7 +10,7 @@
 #include <string.h>
 
 #include <logging/log.h>
-LOG_MODULE_REGISTER(golioth, CONFIG_NET_GOLIOTH_LOG_LEVEL);
+LOG_MODULE_REGISTER(golioth, CONFIG_GOLIOTH_LOG_LEVEL);
 
 #define COAP_BASIC_HEADER_SIZE	4
 
