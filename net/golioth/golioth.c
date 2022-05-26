golioth/golioth-zephyr-sdk

commit 96d31d453881f54bf51881bd757e50a944f6d72a

net: golioth: fix golioth_sendmsg() return code check
Return code was saved to 'ret' variable, but 'err' variable was checked in
the next like instead.

In fact number of sent bytes should be compared with the number returned
from underlying zsock_send() call. Do exactly that inside
golioth_sendmsg(), as that function contains number of total bytes that
should be sent. Change golioth_sendmsg() behavior to return 0 in case of
success and negative value in case of error.

Save value returned from golioth_sendmsg() to 'err', which by convention
can be 0 (success) or negative (error).

Reported-by: Nick Miller <nick@golioth.io>
Signed-off-by: Marcin Niestroj <m.niestroj@emb.dev>

net/golioth/golioth.c
@@ -299,7 +299,13 @@ static int golioth_sendmsg(struct golioth_client *client,
 
 	free(data);
 
-	return ret;
+	if (ret < 0) {
+		return ret;
+	} else if (ret < len) {
+		return -EIO;
+	}
+
+	return 0;
 }
 
 int golioth_send_coap(struct golioth_client *client, struct coap_packet *packet)
@@ -341,7 +347,6 @@ int golioth_send_coap_payload(struct golioth_client *client,
 		.msg_iov = msg_iov,
 		.msg_iovlen = ARRAY_SIZE(msg_iov),
 	};
-	int ret;
 	int err;
 
 	if (client->proto == IPPROTO_UDP) {
@@ -371,7 +376,7 @@ int golioth_send_coap_payload(struct golioth_client *client,
 	msg_iov[1].iov_base = payload;
 	msg_iov[1].iov_len = payload_len;
 
-	ret = golioth_sendmsg(client, &msg, 0);
+	err = golioth_sendmsg(client, &msg, 0);
 	if (err) {
 		return err;
 	}

commit 0c463ade580936e0c1b7d1218d5adbe384d1ad0b

net: golioth: fix golioth_send{,msg}() error path (again)
Both golioth_send() and golioth_sendmsg() use __golioth_send().
__golioth_send() already converts zsock_send() error codes from:

 * -1 (with errno) - in case of error
 * number of bytes sent - in case of success

into:

 * -errno - in case of zsock_send() error
 * 0 - in case of success

Similar logic was implemented in golioth_sendmsg(). Additionally
golioth_sendmsg() checks now for number of bytes actually sent, so that
truncated datagrams can be detected, in which case -EIO is returned. This
behavior is wrong, as __golioth_send() never returns positive values, so
golioth_sendmsg() always ends up returning -EIO.

Fix above issue by moving the check for truncated packets into
__golioth_send() function. It will now return following values:

 * -errno - in case of zsock_send() error
 * -EIO - in case of truncated datagram (less bytes sent than requested)
 * 0 - in case of success

This is already what golioth_sendmsg() tried to return, so just pass the
return value directly to caller, without any further checking in
golioth_sendmsg().

Fixes: 96d31d453881 ("net: golioth: fix golioth_sendmsg() return code
  check")

Signed-off-by: Marcin Niestroj <m.niestroj@emb.dev>

net/golioth/golioth.c
@@ -236,6 +236,8 @@ static int __golioth_send(struct golioth_client *client, uint8_t *data,
 	sent = zsock_send(client->sock, data, len, flags);
 	if (sent < 0) {
 		return -errno;
+	} else if (sent < len) {
+		return -EIO;
 	}
 
 	return 0;
@@ -299,13 +301,7 @@ static int golioth_sendmsg(struct golioth_client *client,
 
 	free(data);
 
-	if (ret < 0) {
-		return ret;
-	} else if (ret < len) {
-		return -EIO;
-	}
-
-	return 0;
+	return ret;
 }
 
 int golioth_send_coap(struct golioth_client *client, struct coap_packet *packet)