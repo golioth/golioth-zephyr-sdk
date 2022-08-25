321fff71f7fd45ac54ddf6d9caccc815c57afcaf
Marcin Niestroj <m.niestroj@emb.dev>
2022-08-25 10:58:56 +0200

    lightdb_helpers: add lightdb_get typed helpers

--- include/net/golioth/lightdb_helpers.h
+++ include/net/golioth/lightdb_helpers.h
@@ -7,12 +7,169 @@
 #ifndef __INCLUDE_NET_GOLIOTH_LIGHTDB_HELPERS_H__
 #define __INCLUDE_NET_GOLIOTH_LIGHTDB_HELPERS_H__
 
+#include <errno.h>
 #include <stdbool.h>
 #include <stdint.h>
 
 #include "lightdb_helpers_priv.h"
 
 struct golioth_client;
+
+/**
+ * @brief Get simple value from Golioth's Light DB
+ *
+ * Get simple (not a structure) value from Golioth's Light DB.
+ *
+ * @param client Golioth instance
+ * @param path Light DB resource path
+ * @param value Value fetched from LightDB
+ *
+ * @retval 0 On success
+ * @retval <0 On failure
+ */
+#define golioth_lightdb_get_auto(_client, _path, _value)	\
+	_Generic((_value),					\
+		 bool *: golioth_lightdb_get_bool,		\
+		 int8_t *: golioth_lightdb_get_int8_t,		\
+		 int16_t *: golioth_lightdb_get_int16_t,	\
+		 int32_t *: golioth_lightdb_get_int32_t,	\
+		 int64_t *: golioth_lightdb_get_int64_t,	\
+		 uint8_t *: golioth_lightdb_get_uint8_t,	\
+		 uint16_t *: golioth_lightdb_get_uint16_t,	\
+		 uint32_t *: golioth_lightdb_get_uint32_t,	\
+		 uint64_t *: golioth_lightdb_get_uint64_t,	\
+		 double *: golioth_lightdb_get_double		\
+	)(_client, _path, _value)
+
+static inline int golioth_lightdb_get_bool(struct golioth_client *client,
+					   const uint8_t *path,
+					   bool *value)
+{
+	return golioth_lightdb_get_basic(client, path, &value,
+					 golioth_lightdb_qcbor_get_bool);
+}
+
+static inline int golioth_lightdb_get_int8_t(struct golioth_client *client,
+					     const uint8_t *path,
+					     int8_t *value)
+{
+	int64_t value_tmp;
+	int err;
+
+	err = golioth_lightdb_get_int64_t_clamp(client, path, &value_tmp,
+						INT8_MIN, INT8_MAX);
+
+	*value = value_tmp;
+
+	return err;
+}
+
+static inline int golioth_lightdb_get_int16_t(struct golioth_client *client,
+					      const uint8_t *path,
+					      int16_t *value)
+{
+	int64_t value_tmp;
+	int err;
+
+	err = golioth_lightdb_get_int64_t_clamp(client, path, &value_tmp,
+						INT16_MIN, INT16_MAX);
+
+	*value = value_tmp;
+
+	return err;
+}
+
+static inline int golioth_lightdb_get_int32_t(struct golioth_client *client,
+					      const uint8_t *path,
+					      int32_t *value)
+{
+	int64_t value_tmp;
+	int err;
+
+	err = golioth_lightdb_get_int64_t_clamp(client, path, &value_tmp,
+						INT32_MIN, INT32_MAX);
+
+	*value = value_tmp;
+
+	return err;
+}
+
+static inline int golioth_lightdb_get_int64_t(struct golioth_client *client,
+					      const uint8_t *path,
+					      int64_t *value)
+{
+	return golioth_lightdb_get_int64_t_clamp(client, path, value,
+						 INT64_MIN, INT64_MAX);
+}
+
+/* static inline int golioth_lightdb_get_int64_t(struct golioth_client *client, */
+/* 					      const uint8_t *path, */
+/* 					      int64_t *value) */
+/* { */
+/* 	return golioth_lightdb_get_basic(client, path, &value, */
+/* 					 golioth_lightdb_qcbor_get_int64_t); */
+/* } */
+
+static inline int golioth_lightdb_get_uint8_t(struct golioth_client *client,
+					      const uint8_t *path,
+					      uint8_t *value)
+{
+	uint64_t value_tmp;
+	int err;
+
+	err = golioth_lightdb_get_uint64_t_clamp(client, path, &value_tmp,
+						 UINT8_MAX);
+
+	*value = value_tmp;
+
+	return err;
+}
+
+static inline int golioth_lightdb_get_uint16_t(struct golioth_client *client,
+					       const uint8_t *path,
+					       uint16_t *value)
+{
+	uint64_t value_tmp;
+	int err;
+
+	err = golioth_lightdb_get_uint64_t_clamp(client, path, &value_tmp,
+						 UINT16_MAX);
+
+	*value = value_tmp;
+
+	return err;
+}
+
+static inline int golioth_lightdb_get_uint32_t(struct golioth_client *client,
+					       const uint8_t *path,
+					       uint32_t *value)
+{
+	uint64_t value_tmp;
+	int err;
+
+	err = golioth_lightdb_get_uint64_t_clamp(client, path, &value_tmp,
+						 UINT32_MAX);
+
+	*value = value_tmp;
+
+	return err;
+}
+
+static inline int golioth_lightdb_get_uint64_t(struct golioth_client *client,
+					       const uint8_t *path,
+					       uint64_t *value)
+{
+	return golioth_lightdb_get_basic(client, path, &value,
+					 golioth_lightdb_qcbor_get_uint64_t);
+}
+
+static inline int golioth_lightdb_get_double(struct golioth_client *client,
+					     const uint8_t *path,
+					     double *value)
+{
+	return golioth_lightdb_get_basic(client, path, &value,
+					 golioth_lightdb_qcbor_get_double);
+}
 
 /**
  * @brief Set simple value to Golioth's Light DB
--- net/golioth/lightdb_helpers.c
+++ net/golioth/lightdb_helpers.c
@@ -5,10 +5,96 @@
  */
 
 #include <qcbor/qcbor.h>
+#include <qcbor/qcbor_spiffy_decode.h>
 #include <qcbor/posix_error_map.h>
 
 #include <net/golioth.h>
 #include <net/golioth/lightdb_helpers.h>
+
+int golioth_lightdb_get_basic(struct golioth_client *client, const uint8_t *path,
+			      void *value_void,
+			      void (*get)(QCBORDecodeContext *dec, void *value_void))
+{
+	QCBORDecodeContext dec;
+	uint8_t buf_raw[16];
+	UsefulBufC buf = { buf_raw, ARRAY_SIZE(buf_raw) };
+	QCBORError qerr;
+	int err;
+
+	err = golioth_lightdb_get(client, path, GOLIOTH_CONTENT_FORMAT_APP_CBOR,
+				  buf_raw, &buf.len);
+	if (err) {
+		return err;
+	}
+
+	QCBORDecode_Init(&dec, buf, QCBOR_DECODE_MODE_NORMAL);
+
+	get(&dec, value_void);
+
+	qerr = QCBORDecode_Finish(&dec);
+	if (qerr != QCBOR_SUCCESS) {
+		return qcbor_error_to_posix(qerr);
+	}
+
+	return 0;
+}
+
+#define LIGHTDB_GET_SIMPLE_DEFINE(_type, _get)				\
+void golioth_lightdb_qcbor_get_##_type(QCBORDecodeContext *dec,		\
+				       void *value_void)		\
+{									\
+	_type *value = value_void;					\
+									\
+	_get(dec, value);						\
+}
+
+LIGHTDB_GET_SIMPLE_DEFINE(bool, QCBORDecode_GetBool);
+LIGHTDB_GET_SIMPLE_DEFINE(int64_t, QCBORDecode_GetInt64);
+LIGHTDB_GET_SIMPLE_DEFINE(uint64_t, QCBORDecode_GetUInt64);
+LIGHTDB_GET_SIMPLE_DEFINE(double, QCBORDecode_GetDouble);
+
+int golioth_lightdb_get_int64_t_clamp(struct golioth_client *client, const uint8_t *path,
+				      int64_t *value, int64_t min, int64_t max)
+{
+	int err;
+
+	err = golioth_lightdb_get_basic(client, path, value,
+					golioth_lightdb_qcbor_get_int64_t);
+	if (err) {
+		return err;
+	}
+
+	if (*value > max) {
+		*value = max;
+		return -ERANGE;
+	}
+
+	if (*value < min) {
+		*value = min;
+		return -ERANGE;
+	}
+
+	return 0;
+}
+
+int golioth_lightdb_get_uint64_t_clamp(struct golioth_client *client, const uint8_t *path,
+				       uint64_t *value, uint64_t max)
+{
+	int err;
+
+	err = golioth_lightdb_get_basic(client, path, value,
+					golioth_lightdb_qcbor_get_uint64_t);
+	if (err) {
+		return err;
+	}
+
+	if (*value > max) {
+		*value = max;
+		return -ERANGE;
+	}
+
+	return 0;
+}
 
 int golioth_lightdb_set_basic(struct golioth_client *client, const uint8_t *path,
 			      const void *value_void,
