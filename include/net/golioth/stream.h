a484f798b821c1918087ae39570a862dc95b9a2a
Marcin Niestroj <m.niestroj@emb.dev>
2022-08-28 23:16:49 +0200

    stream: add new APIs for LightDB Stream
    So far there was no dedicated API for pushing values into LightDB Stream
    service, but instead golioth_lightdb_set(client,
    GOLIOTH_LIGHTDB_STREAM_PATH(path), ...) was used.
    
    Add golioth_stream_push() API to push data into LightDB Stream. This
    works similar to golioth_lightdb_set() and actually takes the same
    parameters.
    
    Add also callback-based equivalent golioth_stream_push_cb().
    
    Signed-off-by: Marcin Niestroj <m.niestroj@emb.dev>

---
+++ include/net/golioth/stream.h
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2022 Golioth, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef GOLIOTH_INCLUDE_NET_GOLIOTH_STREAM_H_
+#define GOLIOTH_INCLUDE_NET_GOLIOTH_STREAM_H_
+
+#include <stdint.h>
+#include <zephyr/net/coap.h>
+#include <net/golioth/req.h>
+
+/**
+ * @defgroup golioth_stream Golioth LightDB Stream
+ * @ingroup net
+ * Functions for interacting with the Golioth LightDB Stream service
+ * @{
+ */
+
+/**
+ * @brief Push value to Golioth's LightDB Stream (callback based)
+ *
+ * Asynchronously push new value to LightDB Stream and let @p cb be invoked when server
+ * acknowledges it or some error condition happens.
+ *
+ * @param[in] client Client instance
+ * @param[in] path LightDB Stream resource path
+ * @param[in] format Format of payload
+ * @param[in] data Payload data
+ * @param[in] data_len Payload length
+ * @param[in] cb Callback executed on response received, timeout or error
+ * @param[in] user_data User data passed to @p cb
+ *
+ * @retval 0 On success
+ * @retval <0 On failure
+ */
+int golioth_stream_push_cb(struct golioth_client *client, const uint8_t *path,
+			   enum golioth_content_format format,
+			   const uint8_t *data, size_t data_len,
+			   golioth_req_cb_t cb, void *user_data);
+
+/**
+ * @brief Push value to Golioth's LightDB Stream (synchronously)
+ *
+ * Synchronously push new value to LightDB Stream.
+ *
+ * @param[in] client Client instance
+ * @param[in] path LightDB Stream resource path
+ * @param[in] format Format of payload
+ * @param[in] data Payload data
+ * @param[in] data_len Payload length
+ *
+ * @retval 0 On success
+ * @retval <0 On failure
+ */
+int golioth_stream_push(struct golioth_client *client, const uint8_t *path,
+			enum golioth_content_format format,
+			const uint8_t *data, size_t data_len);
+
+/** @} */
+
+#endif /* GOLIOTH_INCLUDE_NET_GOLIOTH_STREAM_H_ */
