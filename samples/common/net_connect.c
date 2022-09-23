6ea04950dc27ffaf99cbc85f561d759829f7569d
Marcin Niestroj <m.niestroj@emb.dev>
2022-09-23 16:32:33 +0200

    samples: common: wait for interface being UP
    Wait for interface being UP before any other operation on network
    interface. This makes it sure that network driver is ready for
    wifi_connect() and/or net_dhcpv4_start() calls.
    
    Signed-off-by: Marcin Niestroj <m.niestroj@emb.dev>

--- samples/common/net_connect.c
+++ samples/common/net_connect.c
@@ -12,9 +12,62 @@ LOG_MODULE_REGISTER(golioth_samples, LOG_LEVEL_DBG);
 #include <zephyr/net/net_if.h>
 #include <zephyr/net/dhcpv4.h>
 
+struct wait_data {
+	struct k_sem sem;
+	struct net_mgmt_event_callback cb;
+};
+
+static void event_cb_handler(struct net_mgmt_event_callback *cb,
+			     uint32_t mgmt_event,
+			     struct net_if *iface)
+{
+	struct wait_data *wait = CONTAINER_OF(cb, struct wait_data, cb);
+
+	if (mgmt_event == cb->event_mask) {
+		k_sem_give(&wait->sem);
+	}
+}
+
+static void wait_for_iface_up_event(struct net_if *iface)
+{
+	struct wait_data wait;
+
+	wait.cb.handler = event_cb_handler;
+	wait.cb.event_mask = NET_EVENT_IF_UP;
+
+	k_sem_init(&wait.sem, 0, 1);
+	net_mgmt_add_event_callback(&wait.cb);
+
+	if (!net_if_is_up(iface)) {
+		k_sem_take(&wait.sem, K_FOREVER);
+	}
+
+	net_mgmt_del_event_callback(&wait.cb);
+}
+
+static void wait_for_iface_up_poll(struct net_if *iface)
+{
+	while (!net_if_is_up(iface)) {
+		LOG_DBG("Interface is not up yet!");
+		k_sleep(K_MSEC(100));
+	}
+}
+
+static void wait_for_iface_up(struct net_if *iface)
+{
+	if (IS_ENABLED(CONFIG_NET_MGMT_EVENT)) {
+		wait_for_iface_up_event(iface);
+	} else {
+		wait_for_iface_up_poll(iface);
+	}
+}
+
 void net_connect(void)
 {
 	struct net_if *iface = net_if_get_default();
+
+	LOG_INF("Waiting for interface to be up");
+	wait_for_iface_up(iface);
 
 	if (IS_ENABLED(CONFIG_GOLIOTH_SAMPLE_WIFI)) {
 		LOG_INF("Connecting to WiFi");
