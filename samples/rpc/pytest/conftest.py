3dcb9f5d725e68ec156cda2feca227d8f867935a
Marcin Niestroj <m.niestroj@emb.dev>
2022-10-25 20:07:32 +0200

    samples: rpc: add pytest support
    Add basic pytest runtime test, which tries to call RPC method 'multiply'
    several times.
    
    Signed-off-by: Marcin Niestroj <m.niestroj@emb.dev>

---
+++ samples/rpc/pytest/conftest.py
@@ -0,0 +1,26 @@
+# Copyright (c) 2020 Intel Corporation.
+# Copyright (c) 2022 Golioth, Inc.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import pytest
+
+@pytest.fixture(scope='session')
+def anyio_backend():
+    return 'trio'
+
+# add option "--comdopt" to pytest, or it will report "unknown option"
+# this option is passed from twister.
+def pytest_addoption(parser):
+    parser.addoption(
+        '--cmdopt'
+    )
+    parser.addoption(
+        '--initial-timeout', type=int
+    )
+
+# define fixture to return value of option "--cmdopt", this fixture
+# will be requested by other fixture of tests.
+@pytest.fixture()
+def cmdopt(request):
+    return request.config.getoption('--cmdopt')
