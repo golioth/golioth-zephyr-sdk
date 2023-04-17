d886844e264204dffd4cf8ca0ecb5523437e13a8
Marcin Niestroj <m.niestroj@emb.dev>
2023-04-17 17:43:03 +0200

    samples: lightdb: get: convert pytest script to Python library
    This gives more control over data, timeouts and simplifies code logic a
    bit.
    
    Signed-off-by: Marcin Niestroj <m.niestroj@emb.dev>

--- samples/lightdb/get/pytest/test_sample.py
+++ samples/lightdb/get/pytest/test_sample.py
@@ -1,18 +1,18 @@
-# Copyright (c) 2022 Golioth, Inc.
+# Copyright (c) 2022-2023 Golioth, Inc.
 #
 # SPDX-License-Identifier: Apache-2.0
 
 import base64
-from contextlib import suppress
-import json
 import logging
 import os
-import re
-import subprocess
-import time
 
-import pexpect
+from golioth import Client, Device
 import pytest
+import trio
+
+
+# This is the same as using the @pytest.mark.anyio on all test functions in the module
+pytestmark = pytest.mark.anyio
 
 
 DEFAULT_TIMEOUT = 30
@@ -26,66 +26,41 @@ def initial_timeout(request):
     return timeout
 
 
-def device_name():
-    return os.environ["GOLIOTH_DEVICE_NAME"]
+@pytest.fixture(scope='session')
+async def device():
+    client = Client(os.environ.get("GOLIOTHCTL_CONFIG"))
+    project = await client.default_project()
+    return await project.device_by_name(os.environ['GOLIOTH_DEVICE_NAME'])
 
 
-def goliothctl_args():
-    args = []
-
-    with suppress(KeyError):
-        args += ['-c', os.environ["GOLIOTHCTL_CONFIG"]]
-
-    with suppress(KeyError):
-        args += pexpect.split_command_line(os.environ["GOLIOTHCTL_OPTS"])
-
-    return args
-
-
-def counter_set(value: int):
+async def counter_set(device: Device, value: int):
     logging.info("Setting /counter to %s", value)
-    subprocess.run(["goliothctl"] + goliothctl_args() + ["lightdb", "set", device_name(), "/counter", "-b", str(value)],
-                   check=True)
+    await device.lightdb.set('counter', value)
 
 
-def goliothctl_readline(goliothctl, timeout):
-    index = goliothctl.expect([goliothctl.crlf, goliothctl.delimiter],
-                              timeout=timeout)
-    if index == 0:
-        return goliothctl.before + goliothctl.crlf
-    else:
-        return goliothctl.before
-
-
-def test_lightdb_counter_get(initial_timeout):
+async def test_lightdb_counter_get(initial_timeout, device):
     magic_value = 8664100
     expected_updates = 5
 
-    try:
-        counter_expected = magic_value
-        counter_set(counter_expected)
+    counter_expected = magic_value
 
-        args = goliothctl_args() + ["logs", "listen", "--json", device_name()]
+    await counter_set(device, counter_expected)
 
-        logging.info("running goliothctl with args=%s", args)
-        goliothctl = pexpect.spawn("goliothctl", args)
+    # Allow much more time before first log entry arrives
+    timeout = initial_timeout
 
-        # Allow much more time before first log entry arrives
-        timeout = initial_timeout
-
+    async with device.logs_monitor() as logs_monitor:
         while magic_value + expected_updates > counter_expected:
-            line = goliothctl_readline(goliothctl, timeout)
-            entry = json.loads(line)
+            with trio.fail_after(timeout):
+                log = await logs_monitor.get()
 
             # Once we are connected (log entries arrived), reduce timeout value
             timeout = DEFAULT_TIMEOUT
 
-            assert "message" in entry, f"No 'message' in {entry}"
-
-            if "Counter" not in entry["message"]:
+            if "Counter" not in log.message:
                 continue
 
-            binary_value = base64.decodebytes(entry["metadata"]["hexdump"].encode())
+            binary_value = base64.decodebytes(log.metadata["hexdump"].encode())
             counter_logged = int(binary_value.decode())
 
             logging.info("Logged %s (expected %s)", counter_logged, counter_expected)
@@ -94,8 +69,6 @@ def test_lightdb_counter_get(initial_timeout):
                 counter_expected += 1
 
                 # Workaround LightDB issue, which does not consume requests too often
-                time.sleep(1)
+                await trio.sleep(1)
 
-                counter_set(counter_expected)
-    finally:
-        goliothctl.terminate()
+                await counter_set(device, counter_expected)
