0fc34c939abfa43a9fcff12e003e81904bda830f
Marcin Niestroj <m.niestroj@emb.dev>
2023-04-19 00:18:59 +0200

    samples: lightdb_stream: convert pytest script to Python library
    This gives more control over data, timeouts and simplifies code logic a
    bit.
    
    Signed-off-by: Marcin Niestroj <m.niestroj@emb.dev>

--- samples/lightdb_stream/pytest/test_sample.py
+++ samples/lightdb_stream/pytest/test_sample.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2022 Golioth, Inc.
+# Copyright (c) 2022-2023 Golioth, Inc.
 #
 # SPDX-License-Identifier: Apache-2.0
 
@@ -7,8 +7,13 @@ import json
 import logging
 import os
 
-import pexpect
+from golioth import Client, Device
 import pytest
+import trio
+
+
+# This is the same as using the @pytest.mark.anyio on all test functions in the module
+pytestmark = pytest.mark.anyio
 
 
 DEFAULT_TIMEOUT = 30
@@ -22,43 +27,21 @@ def initial_timeout(request):
     return timeout
 
 
-def goliothctl_args():
-    args = []
-
-    with suppress(KeyError):
-        args += ['-c', os.environ["GOLIOTHCTL_CONFIG"]]
-
-    with suppress(KeyError):
-        args += pexpect.split_command_line(os.environ["GOLIOTHCTL_OPTS"])
-
-    return args
+@pytest.fixture(scope='session')
+async def device():
+    client = Client(os.environ.get("GOLIOTHCTL_CONFIG"))
+    project = await client.default_project()
+    return await project.device_by_name(os.environ['GOLIOTH_DEVICE_NAME'])
 
 
-def goliothctl_readline(goliothctl, timeout):
-    index = goliothctl.expect([goliothctl.crlf, goliothctl.delimiter],
-                              timeout=timeout)
-    if index == 0:
-        return goliothctl.before + goliothctl.crlf
-    else:
-        return goliothctl.before
-
-
-def test_stream_events_received(initial_timeout):
-    try:
-        args = goliothctl_args() + ["stream", "listen"]
-
-        with suppress(KeyError):
-            args.append(os.environ["GOLIOTH_DEVICE_NAME"])
-
-        logging.info("running goliothctl with args=%s", args)
-        goliothctl = pexpect.spawn("goliothctl", args)
-
+async def test_stream_events_received(initial_timeout, device):
+    async with device.stream.monitor() as stream:
         # Allow much more time before first log entry arrives
         timeout = initial_timeout
 
         for i in range(0, 5):
-            line = goliothctl_readline(goliothctl, timeout)
-            entry = json.loads(line)
+            with trio.fail_after(timeout):
+                entry = await stream.get()
 
             # Once we are connected (stream entries arrived), reduce timeout value
             timeout = DEFAULT_TIMEOUT
@@ -74,5 +57,3 @@ def test_stream_events_received(initial_timeout):
             assert 0 < temp and temp < 80, "Temperature value is insane"
 
             logging.info("Temperature %-5s at %s", temp, ts)
-    finally:
-        goliothctl.terminate()
