7b6656b6938359ca247d28839391894200fa1d17
Marcin Niestroj <m.niestroj@emb.dev>
2023-04-18 22:48:05 +0200

    samples: lightdb_led: convert pytest script to Python library
    This gives more control over data, timeouts and simplifies code logic a
    bit.
    
    Signed-off-by: Marcin Niestroj <m.niestroj@emb.dev>

--- samples/lightdb_led/pytest/test_sample.py
+++ samples/lightdb_led/pytest/test_sample.py
@@ -1,17 +1,19 @@
-# Copyright (c) 2022 Golioth, Inc.
+# Copyright (c) 2022-2023 Golioth, Inc.
 #
 # SPDX-License-Identifier: Apache-2.0
 
-from contextlib import suppress
-import json
 import logging
 import os
 import re
-import subprocess
-import time
+from typing import Iterable
 
-import pexpect
+from golioth import Client, Device
 import pytest
+import trio
+
+
+# This is the same as using the @pytest.mark.anyio on all test functions in the module
+pytestmark = pytest.mark.anyio
 
 
 DEFAULT_TIMEOUT = 30
@@ -25,73 +27,43 @@ def initial_timeout(request):
     return timeout
 
 
-def device_name():
-    return os.environ["GOLIOTH_DEVICE_NAME"]
+@pytest.fixture(scope='session')
+async def device():
+    client = Client(os.environ.get("GOLIOTHCTL_CONFIG"))
+    project = await client.default_project()
+    return await project.device_by_name(os.environ['GOLIOTH_DEVICE_NAME'])
 
 
-def goliothctl_args():
-    args = []
-
-    with suppress(KeyError):
-        args += ['-c', os.environ["GOLIOTHCTL_CONFIG"]]
-
-    with suppress(KeyError):
-        args += pexpect.split_command_line(os.environ["GOLIOTHCTL_OPTS"])
-
-    return args
+async def leds_set(device: Device, leds: Iterable[bool]):
+    logging.info("Setting /led to %s", leds)
+    await device.lightdb.set('led', {i: led for i, led in enumerate(leds)})
 
 
-def leds_set(leds):
-    value = json.dumps({str(i): v for i, v in enumerate(leds)})
-    logging.info("Setting /led to %s", value)
-    subprocess.run(["goliothctl"] + goliothctl_args() + ["lightdb", "set", device_name(), f"/led", "-b", value],
-                   check=True)
-
-
-def goliothctl_readline(goliothctl, timeout):
-    index = goliothctl.expect([goliothctl.crlf, goliothctl.delimiter],
-                              timeout=timeout)
-    if index == 0:
-        return goliothctl.before + goliothctl.crlf
-    else:
-        return goliothctl.before
-
-
-def test_lightdb_led(initial_timeout):
+async def test_lightdb_led(initial_timeout, device):
     num_leds = 4
     num_leds_on = 0
-    msg_pattern = re.compile(r"LED (\d+) -> (O[NF]+)")
+    msg_pattern = re.compile(r"LED (?P<id>\d+) -> (?P<state>O[NF]+)")
     leds = [False for i in range(num_leds)]
 
-    try:
-        args = goliothctl_args() + ["logs", "listen", "--json", device_name()]
-
-        logging.info("running goliothctl with args=%s", args)
-        goliothctl = pexpect.spawn("goliothctl", args)
-
-        time.sleep(2) # Wait 2s to be (almost) sure that logs will show LED updates
-        leds_set(leds)
+    async with device.logs_monitor() as logs_monitor:
+        await trio.sleep(2) # Wait 2s to be (almost) sure that logs will show LED updates
+        await leds_set(device, leds)
 
         # Allow much more time before first log entry arrives
         timeout = initial_timeout
 
         while True:
-            line = goliothctl_readline(goliothctl, timeout)
-            entry = json.loads(line)
+            with trio.fail_after(timeout):
+                log = await logs_monitor.get()
 
             # Once we are connected (log entries arrived), reduce timeout value
             timeout = DEFAULT_TIMEOUT
 
-            assert "message" in entry, f"No 'message' in {entry}"
-
-            msg = msg_pattern.match(entry["message"])
+            msg = msg_pattern.match(log.message)
             if not msg:
                 continue
 
-            led_id = int(msg[1])
-            led_on = True if msg[2] == "ON" else False
-
-            leds[led_id] = led_on
+            leds[int(msg['id'])] = msg['state'] == 'ON'
 
             logging.info("Logged %s (expected %s)", leds,
                          [True] * num_leds_on + [False] * (num_leds - num_leds_on))
@@ -105,8 +77,6 @@ def test_lightdb_led(initial_timeout):
                 leds[num_leds_on - 1] = True
 
                 # Workaround LightDB issue, which does not consume requests too often
-                time.sleep(1)
+                await trio.sleep(1)
 
-                leds_set(leds)
-    finally:
-        goliothctl.terminate()
+                await leds_set(device, leds)
