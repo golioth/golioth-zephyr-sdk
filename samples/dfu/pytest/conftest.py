4ac79dafa7ea087a8a76fa9f3f34f271de5cff13
Marcin Niestroj <m.niestroj@emb.dev>
2023-06-01 19:59:27 +0200

    bump Zephyr to 3.4.0+
    MINIMAL_LIBC_MALLOC_ARENA_SIZE got deprecated in favor of
    COMMON_LIBC_MALLOC_ARENA_SIZE, so update that.
    
    Rework pytest testcases to use `dut` fixture, which is needed in order to
    flash device before running test.
    
    Tip of main (as opposed to 3.4.0 release) contains eventfd related fixes,
    which are needed in order to wakeup poll() using eventfd_write().
    
    --cmdopt is no longer passed to pytest invocation from twister. Use
    request.config.build_dir instead.
    
    Add timeouts to sample.yaml configurations, as from now on pytest twister
    harness respects that timeout (which was not entirely true with the initial
    pytest support).
    
    Use 'hal_espressif' revision that contains fix for duplicated enum. This is
    just a temporary solution, as this revision is not used yet in Zephyr's
    west manifest.
    
    Signed-off-by: Marcin Niestroj <m.niestroj@emb.dev>

--- samples/dfu/pytest/conftest.py
+++ samples/dfu/pytest/conftest.py
@@ -12,36 +12,16 @@ GOLIOTH_BASE = Path(__file__).resolve().parents[3]
 ZEPHYR_BASE = GOLIOTH_BASE.parents[2] / 'zephyr'
 
 sys.path.insert(0, str(ZEPHYR_BASE / 'scripts' / 'west_commands'))
-from runners.core import BuildConfiguration
-
-PROJECT_NAME = "dfu"
+from build_info import BuildInfo
 
 @pytest.fixture(scope='session')
 def anyio_backend():
     return 'trio'
 
-# add option "--comdopt" to pytest, or it will report "unknown option"
-# this option is passed from twister.
-def pytest_addoption(parser):
-    parser.addoption(
-        '--cmdopt'
-    )
-
-# define fixture to return value of option "--cmdopt", this fixture
-# will be requested by other fixture of tests.
 @pytest.fixture(scope='session')
-def cmdopt(request):
-    return request.config.getoption('--cmdopt')
+def build_info(request):
+    return BuildInfo(Path(request.config.option.build_dir))
 
 @pytest.fixture(scope='session')
-def build_conf(cmdopt):
-    if (Path(cmdopt) / "pm.config").is_file():
-        return BuildConfiguration(cmdopt)
-    elif (Path(cmdopt) / PROJECT_NAME).is_dir():
-        return BuildConfiguration(str(Path(cmdopt) / PROJECT_NAME))
-
-    raise RuntimeError("Unsupported build directory structure")
-
-@pytest.fixture(scope='session')
-def initial_timeout(build_conf):
-    return build_conf['CONFIG_GOLIOTH_SAMPLE_TEST_CONNECT_TIMEOUT']
+def initial_timeout(build_info: BuildInfo):
+    return build_info.conf['CONFIG_GOLIOTH_SAMPLE_TEST_CONNECT_TIMEOUT']
